0b4e0fbaa270f4f3f2fd76d407ccfdb322121c07
sanecheqq <semin.alex222@yandex.ru>
2024-04-02 17:54:45 +0300

    немного улучшен GenderType

--- tg-client/src/main/java/com/liga/semin/tgclient/model/GenderType.java
+++ tg-client/src/main/java/com/liga/semin/tgclient/model/GenderType.java
@@ -1,7 +1,21 @@
 package com.liga.semin.tgclient.model;
 
 public enum GenderType {
-    MALE,
-    FEMALE,
-    ALL
+    MALE("Сударъ"),
+    FEMALE("Сударыня"),
+    ALL("Все");
+
+    private String gender;
+    GenderType(String gender) {
+        this.gender = gender;
+    }
+
+    public static GenderType getGenderType(String gender) {
+        return switch (gender) {
+            case "Сударъ" -> MALE;
+            case "Сударыня" -> FEMALE;
+            case "Все" -> ALL;
+            default -> throw new IllegalArgumentException("Неверный пол");
+        };
+    }
 }
--- tg-client/src/main/java/com/liga/semin/tgclient/model/UserDto.java
+++ tg-client/src/main/java/com/liga/semin/tgclient/model/UserDto.java
@@ -2,12 +2,14 @@ package com.liga.semin.tgclient.model;
 
 import lombok.Getter;
 import lombok.Setter;
+import lombok.ToString;
 
 /**
  * Класс для временного хранения данных юзера в процессе регистрации, а также для отправки запросов на сервер.
  */
 @Getter
 @Setter
+@ToString
 public class UserDto {
     private long id;
     private GenderType userGender;
