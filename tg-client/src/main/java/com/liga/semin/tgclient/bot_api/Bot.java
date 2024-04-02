f51470ac3ddf6c77446b07e2cf0aead3fbab6668
sanecheqq <semin.alex222@yandex.ru>
2024-04-02 16:56:14 +0300

    добавлена регистрация бота; также решил изменить типа бота на LongPolling

--- tg-client/src/main/java/com/liga/semin/tgclient/bot_api/Bot.java
+++ tg-client/src/main/java/com/liga/semin/tgclient/bot_api/Bot.java
@@ -3,34 +3,36 @@ package com.liga.semin.tgclient.bot_api;
 import com.liga.semin.tgclient.bot_config.BotConfig;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
-import org.telegram.telegrambots.bots.TelegramWebhookBot;
+import org.telegram.telegrambots.bots.TelegramLongPollingBot;
 import org.telegram.telegrambots.meta.api.methods.BotApiMethod;
 import org.telegram.telegrambots.meta.api.objects.Update;
+import org.telegram.telegrambots.meta.exceptions.TelegramApiException;
 
 @Component
-public class Bot extends TelegramWebhookBot {
+public class Bot extends TelegramLongPollingBot {
     private final BotConfig botConfig;
-
+    private final UserStateResolver userStateResolver;
 
     @Autowired
-    public Bot(BotConfig botConfig) {
+    public Bot(BotConfig botConfig, UserStateResolver userStateResolver) {
         super(botConfig.getToken());
         this.botConfig = botConfig;
-    }
-
-    @Override
-    public BotApiMethod<?> onWebhookUpdateReceived(Update update) {
-
-        return null;
-    }
-
-    @Override
-    public String getBotPath() {
-        return botConfig.getWebhook();
+        this.userStateResolver = userStateResolver;
     }
 
     @Override
     public String getBotUsername() {
         return botConfig.getBotName();
     }
+
+    @Override
+    public void onUpdateReceived(Update update) {
+        BotApiMethod<?> botApiMethod = userStateResolver.resolveUser(update);
+        try {
+            execute(botApiMethod);
+        } catch (TelegramApiException e) {
+            throw new RuntimeException(e);
+        }
+    }
+
 }
--- tg-client/src/main/java/com/liga/semin/tgclient/bot_config/BotConfig.java
+++ tg-client/src/main/java/com/liga/semin/tgclient/bot_config/BotConfig.java
@@ -1,5 +1,6 @@
 package com.liga.semin.tgclient.bot_config;
 
+import com.liga.semin.tgclient.bot_api.Bot;
 import lombok.Getter;
 import lombok.Setter;
 import org.springframework.beans.factory.annotation.Value;
@@ -7,6 +8,9 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.client.RestTemplate;
+import org.telegram.telegrambots.meta.TelegramBotsApi;
+import org.telegram.telegrambots.meta.exceptions.TelegramApiException;
+import org.telegram.telegrambots.updatesreceivers.DefaultBotSession;
 
 @Configuration
 @Getter
@@ -19,13 +23,16 @@ public class BotConfig {
     @Value("${bot.token}")
     private String token;
 
-    @Value("${bot.webhoot}")
-    private String webhook;
-
     @Bean
     public RestTemplate restTemplate() {
         return new RestTemplate();
     }
 
+    @Bean
+    public TelegramBotsApi telegramBotsApi(Bot bot) throws TelegramApiException {
+        var api = new TelegramBotsApi(DefaultBotSession.class);
+        api.registerBot(bot);
+        return api;
+    }
 
 }
