6dd842e825e49e341cc40229a41cfa6f1e06a025
sanecheqq <semin.alex222@yandex.ru>
2024-04-03 11:23:30 +0300

    добавлен контроллер с поинтами создания, получения, удаления пользака; лайка другого пользака

---
+++ server/src/main/java/com/liga/semin/server/controller/UserController.java
@@ -0,0 +1,41 @@
+package com.liga.semin.server.controller;
+
+import com.liga.semin.server.message.PostFavoriteResponse;
+import com.liga.semin.server.message.UserDto;
+import com.liga.semin.server.service.UserService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequestMapping("api/v1/users")
+@RequiredArgsConstructor
+public class UserController {
+    private final UserService userService;
+
+    @PostMapping
+    public ResponseEntity<UserDto> createUser(@RequestBody UserDto user) {
+        return ResponseEntity.ok(userService.createUser(user));
+    }
+
+    @GetMapping("{id}")
+    public ResponseEntity<UserDto> getUser(@PathVariable("id") Long id) {
+        return ResponseEntity.ok(userService.getUser(id));
+    }
+    // todo: update user ???
+    @DeleteMapping("{id}")
+    public ResponseEntity<?> deleteUser(@PathVariable("id") Long id) {
+        userService.deleteUser(id);
+        return ResponseEntity.ok().build();
+    }
+
+    @PostMapping("/favorite")
+    public ResponseEntity<PostFavoriteResponse> postFavorite(
+            @RequestParam(name = "from") Long from,
+            @RequestParam(name = "to") Long to
+    ) {
+        return ResponseEntity.ok(
+                new PostFavoriteResponse(userService.postFavorite(from, to))
+        );
+    }
+}
